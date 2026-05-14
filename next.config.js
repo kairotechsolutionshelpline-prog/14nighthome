673adfefc0d0803d83ca25a1008c0f0d2ef002dc
Legacy Cartel <yourname@gmail.com>
2026-05-15 01:05:32 +0530

    fix DATABASE_URL runtime config

--- next.config.js
+++ next.config.js
@@ -1,37 +1,21 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
-
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
   async headers() {
     return [
       {
         source: '/(.*)',
-
         headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-
-          {
-            key: 'Permissions-Policy',
-            value: 'camera=(), microphone=()',
-          },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          { key: 'Permissions-Policy', value: 'camera=(), microphone=()' },
         ],
       },
     ]
   },
 }
-
 module.exports = nextConfig
